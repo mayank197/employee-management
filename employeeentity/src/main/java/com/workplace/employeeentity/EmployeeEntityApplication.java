9db4153b0ce08a48ffb503a5fface35f934b32de
Mayank Sharma <mayank.1996.2014@gmail.com>
2020-06-27 14:23:45 +0530

    Removed Redundancies

---
+++ employeeentity/src/main/java/com/workplace/employeeentity/EmployeeEntityApplication.java
@@ -0,0 +1,12 @@
+package com.workplace.employeeentity;
+
+/*
+    File Name : EmployeeEntityApplication.java
+    
+    @author Mayank Sharma
+    First Created on 27-06-2020 at 14:15
+*/
+
+
+public class EmployeeEntityApplication {
+}
