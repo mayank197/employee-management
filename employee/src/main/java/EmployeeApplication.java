06ae0f47d24b91de631f1807c8f1fb7c56ea992c
Mayank Sharma <mayank.1996.2014@gmail.com>
2020-06-27 13:40:07 +0530

    Initial Setup

---
+++ employee/src/main/java/EmployeeApplication.java
@@ -0,0 +1,18 @@
+/*
+    File Name : EmployeeApplication.java
+    
+    @author Mayank Sharma
+    First Created on 27-06-2020 at 13:37
+*/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeApplication.class,args);
+    }
+
+}
