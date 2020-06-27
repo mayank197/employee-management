139d8f735691964d2742ee6a9456d19790e30754
Mayank Sharma <mayank.1996.2014@gmail.com>
2020-06-27 14:29:39 +0530

    Removed Initial Setup Blockers

--- employeeentity/src/main/java/com/workplace/employeeentity/EmployeeEntityApplication.java
+++ employeeentity/src/main/java/com/workplace/employeeentity/EmployeeEntityApplication.java
@@ -8,5 +8,14 @@ package com.workplace.employeeentity;
 */
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class EmployeeEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeEntityApplication.class,args);
+    }
+
 }
